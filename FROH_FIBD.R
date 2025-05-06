f64273d4fe1d7ebd357001a1115d6e8ade6913c3
odawage <odabraar@gmail.com>
2025-05-06 09:21:14 +0200

    more updated filesinfo[ , 2] <- as.numeric(data)

--- FROH_FIBD.R
+++ FROH_FIBD.R
@@ -4,223 +4,168 @@
 #   Both overall and for a specific BIN
 # =====================
 
-
 # Loading packages
 # ======================
+.libPaths("/mnt/users/odwa/R/myLib" )
 library(tidyverse)
 library(data.table)
+library(foreach)
+library(doSNOW)
+library(doParallel)
 
-# Loading the scenario list
+# Cluster
+# ===============
+# Number of cores to use
+num_cores <- as.integer(Sys.getenv("SLURM_CPUS_PER_TASK", unset = 1))
+print(paste0("CPUs available: ", num_cores))
+# Use one less than available cores
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+clusterEvalQ(cl, {
+  .libPaths("/mnt/users/odwa/R/myLib" )
+  library(tidyverse)
+  library(data.table)
+  library(foreach)
+  library(doSNOW)
+  library(doParallel)
+})
+
+# Combos
 # ====================
+proj.dir = "/mnt/project/SimData/Paper_1"
 
-scenario_tbl <- read_table("Scenarios",
-                           col_names = FALSE)[1:40,]
-colnames(scenario_tbl) <- c("Dataset","Density","Setting")
+NEs = c("NE100","NE200")
+Reps = c(1:10)
+Densities = c("Full_Dens","Half_Dens")
+Methods = c("Meyermans","Default","Norm", "Norm_small")
 
-datasets <- unique(scenario_tbl$Dataset)
-
-
-
-# Calculating per individual FROH
-# =================
-
-ROH_ind_all <- data.frame()
-IBD_ind_all <- data.frame()
-
-# I'm looping over dataset cause it 
-# it's the remnants of previous version 
-for (dataset in unique(scenario_tbl$Dataset)  ) {
-
-  temp_scen <- scenario_tbl %>% 
-    filter(Dataset == dataset)
-  
-  
-  for (i in 1:8) {
-    setting <- temp_scen$Setting[i]
-    density <- temp_scen$Density[i]
-    
-    # To get the right proportion of SNPs I have to choose different marker maps
-    if (density == "Half") {
-     markerPosition_kb <- read_csv(paste0("Dataset/",dataset,"/50_markerPositionsRep.res.bz2"),show_col_types = FALSE) %>%
-       mutate(position_kb = BPP/1000)
-    geno_length <-  markerPosition_kb$position_kb[nrow(markerPosition_kb)] - markerPosition_kb$position_kb[1]
-    N_SNPs <- nrow(markerPosition_kb)
-    }else{
-      markerPosition_kb <- read_csv(paste0("Dataset/",dataset,"/R_output/markerPosition_kb.txt"),show_col_types = FALSE)%>%
-        mutate(position_kb = position_bpp/1000)
-      
-      geno_length <-  markerPosition_kb$position_kb[nrow(markerPosition_kb)] - markerPosition_kb$position_kb[1]
-      N_SNPs <- nrow(markerPosition_kb)
-    }
-    
-    
-    ROH_edit <- read_csv(paste0("Dataset/",dataset,"/R_output/",density,"_Dens/",setting,"/ROH_edit.txt"),show_col_types = FALSE)
-    
-    ROH_ind <- ROH_edit %>% 
-      group_by(ID) %>% 
-      summarise(ROH_SNPs = sum(NSNP),
-                sum_kb = sum(KB),
-                F_ROH_snp = ROH_SNPs/N_SNPs,       # This is the one used in the Paper 
-                F_ROH_kb = sum_kb/geno_length) %>% 
-      mutate(dataset = dataset, # Showing in the scenario information 
-             density = density,
-             setting = setting)
-    
-    ROH_ind_all <- rbind(ROH_ind_all, ROH_ind)
-    
-  } #set_dens
-  cat(dataset, "\n")
-}
-
-
-
-write.table(x=ROH_ind_all ,file= paste("./Dataset/Correlation/F_ROH.txt", sep = ""),row.names=FALSE,quote=FALSE)
-
-
-# ==========
-#
-# Then repeat for FIBD
-#
-# =============
-
-IBD_ind_all <- data.frame()
-
-for (dataset in unique(scenario_tbl$Dataset)  ) {
-  
-  
-  true_IBD_position <- read_csv(paste0("Dataset/",dataset,"/R_output/true_IBD_position.txt"),show_col_types = FALSE)
-  IBD_geno_length <-  true_IBD_position$position_bpp[nrow(true_IBD_position)] - true_IBD_position$position_bpp[1]
-  N_IBD_SNPs <- nrow(true_IBD_position)
-  
-  
-  info_ADAM_roh_bin <- read_csv(paste0("Dataset/",dataset,"/IBD_segments/info_ADAM_roh_bin.txt"),show_col_types = FALSE) %>% 
-    mutate(NSNPS = stop -start, 
-           BPP = stop_bpp - start_bpp) 
-  
-  IBD_ind <- info_ADAM_roh_bin %>% 
-    group_by(ID) %>% 
-    summarise(IBD_SNPs = sum(NSNPS),
-              sum_IBD_BPP = sum(BPP),
-              F_IBD_snp = IBD_SNPs/N_IBD_SNPs, # Useing this one in the paper
-              F_IBD_BPP = sum_IBD_BPP/IBD_geno_length) %>% 
-    mutate(dataset = dataset)
-  
-  IBD_ind_all <- rbind(IBD_ind_all, IBD_ind)
-}
-
-
-write.table(x= IBD_ind_all ,file= paste("./Dataset/Correlation/F_IBD.txt", sep = ""),row.names=FALSE,quote=FALSE)
-
-
-
+combinations_df <- expand.grid(NE = NEs , Rep = Reps)
+combinations_all <- expand.grid(NE = NEs , Rep = Reps,Dens = Densities, Method = Methods)
 
 # ===============
 #   The same code grouped by bin as well 
 #   Meaning that the split by bin code is added 
 #==============
 
-
-
-ROH_ind_all <- data.frame()
 IBD_ind_all <- data.frame()
 
-for (dataset in unique(scenario_tbl$Dataset)  ) {
+
+# Parallel loop     
+IBD_ind_all_list <- foreach(i = 1:nrow(combinations_df), .packages = c('data.table', 'tidyverse' )) %dopar% {
   
+  # Get the current combination
+  NE <- combinations_df$NE[i]
+  Rep <- combinations_df$Rep[i]
   
-  true_IBD_position <- read_csv(paste0("Dataset/",dataset,"/R_output/true_IBD_position.txt"),show_col_types = FALSE)
-  IBD_geno_length <-  (true_IBD_position$position_kb[nrow(true_IBD_position)] - true_IBD_position$position_kb[1])/1000
+  data.dir = paste0(proj.dir,"/",NE,"/Rep",Rep)
+
+  true_IBD_position <- read_csv(file.path(data.dir,"true_IBD_position.txt"),show_col_types = FALSE)
+  IBD_geno_length <-  (true_IBD_position$position_bpp[nrow(true_IBD_position)] - true_IBD_position$position_bpp[1])/1000
   N_IBD_SNPs <- nrow(true_IBD_position)
-  
-  
-  
-  info_ADAM_roh_bin <- read_csv(paste0("Dataset/",dataset,"/IBD_segments/info_ADAM_roh_bin.txt"),show_col_types = FALSE) %>% 
-    mutate(NSNPS = stop -start, 
-           KB = (stop_bpp - start_bpp)/1000) 
+
+  info_ADAM_roh_bin <- read_csv(file.path(data.dir,"info_ADAM_roh_bin.txt"),show_col_types = FALSE) %>% 
+    mutate(KB = (stop_bpp - start_bpp)/1000) 
   
   # Make the BINS 
-  
   bin_vec <- vector()
-  
   # Define breakpoints and labels
   breakpoints <- c(0, 1000, 2000, 3000, 4000, 5000, 9000, Inf)
   labels <- c("0_1MB", "1_2MB", "2_3MB", "3_4MB", "4_5MB", "5_9MB", "9MB+")
-  
   # Use cut to create the bin_vec
   bin_vec <- cut(info_ADAM_roh_bin$KB, breaks = breakpoints, labels = labels, right = FALSE)
-  
   bin_vec_fac_2 <- factor(bin_vec, levels = c("0_1MB", "1_2MB" , "2_3MB" , "3_4MB" , "4_5MB","5_9MB","9MB+" ))
-  
   info_ADAM_roh_bin$BIN <- bin_vec_fac_2
-  
-  
+
   # Group and summarise
+  F_tot <- info_ADAM_roh_bin %>% 
+    group_by(ID) %>% 
+    dplyr::summarise(IBD_SNPs = sum(NSNP),
+                     sum_IBD_kb = sum(KB),
+                     F_IBD_snp = IBD_SNPs/N_IBD_SNPs,
+                     F_IBD_kb = sum_IBD_kb/IBD_geno_length) %>% 
+    mutate(BIN = "Total",
+           NE = NE,
+           Rep = Rep)
+  
   IBD_ind <- info_ADAM_roh_bin %>% 
     group_by(ID, BIN) %>% 
-    summarise(IBD_SNPs = sum(NSNPS),
+    dplyr::summarise(IBD_SNPs = sum(NSNP),
               sum_IBD_kb = sum(KB),
               F_IBD_snp = IBD_SNPs/N_IBD_SNPs,
               F_IBD_kb = sum_IBD_kb/IBD_geno_length) %>% 
-    mutate(dataset = dataset)
+    mutate(NE = NE,
+           Rep = Rep)
+  IBD_ind <-rbind(IBD_ind, F_tot)
+  return(IBD_ind)
+  #cat(dataset, "\n")
+} 
+
+IBD_ind_all <- do.call(rbind, IBD_ind_all_list)
+
+# ==============
+#   Rep BIN FROH 
+# ===============
+
+
+ROH_ind_all_list <- foreach(i = 1:nrow(combinations_all), .packages = c('data.table', 'tidyverse' )) %dopar% {
+
+  NE = combinations_all$NE[i]
+  Rep = combinations_all$Rep[i]
+  Dens = combinations_all$Dens[i]
+  Method = combinations_all$Method[i]
   
-  IBD_ind_all <- rbind(IBD_ind_all, IBD_ind)
+  dens.dir = paste0(proj.dir,"/",NE,"/Rep",Rep,"/",Dens)
+  roh.dir = paste0(proj.dir,"/",NE,"/Rep",Rep,"/",Dens,"/",Method)
+  
+  markerPosition_kb <- read_csv(file.path(dens.dir,"markerPositionsRep1.res.bz2"),show_col_types = FALSE) %>%
+    mutate(position_kb = BPP/1000)
+  geno_length <-  markerPosition_kb$position_kb[nrow(markerPosition_kb)] - markerPosition_kb$position_kb[1]
+  N_SNPs <- nrow(markerPosition_kb)
+
+  
+  ROH_edit <- fread(file.path(roh.dir,"ROH_analyse.hom")) %>% 
+    dplyr::rename(ID = "IID")
+  
+  # Make the BINS
+  bin_vec <- vector()
+  # Define breakpoints and labels
+  breakpoints <- c(0, 1000, 2000, 3000, 4000, 5000, 9000, Inf)
+  labels <- c("0_1MB", "1_2MB", "2_3MB", "3_4MB", "4_5MB", "5_9MB", "9MB+")
+  # Use cut to create the bin_vec
+  bin_vec <- cut(ROH_edit$KB, breaks = breakpoints, labels = labels, right = FALSE)
+  bin_vec_fac_2 <- factor(bin_vec, levels = c("0_1MB", "1_2MB" , "2_3MB" , "3_4MB" , "4_5MB","5_9MB","9MB+" ))
+  ROH_edit$BIN <- bin_vec_fac_2
   
   
-  temp_scen <- scenario_tbl %>% 
-    filter(Dataset == dataset)
+  # summarise
+  F_tot <- ROH_edit %>% 
+    group_by(ID) %>% 
+    dplyr::summarise(ROH_SNPs = sum(NSNP),
+                     sum_ROH_kb = sum(KB),
+                     F_ROH_snp = ROH_SNPs/N_SNPs,
+                     F_ROH_kb = sum_ROH_kb/geno_length) %>% 
+    mutate(BIN = "Total",
+           NE = NE,
+           Rep = Rep, 
+           Dens = Dens,
+           Method = Method)
   
+  ROH_ind <- ROH_edit %>% 
+    group_by(ID, BIN) %>% 
+    dplyr::summarise(ROH_SNPs = sum(NSNP),
+              sum_ROH_kb = sum(KB),
+              F_ROH_snp = ROH_SNPs/N_SNPs,
+              F_ROH_kb = sum_ROH_kb/geno_length) %>% 
+    mutate(NE = NE,
+           Rep = Rep, 
+           Dens = Dens,
+           Method = Method)
   
-  for (i in 1:8) {
-    setting <- temp_scen$Setting[i]
-    density <- temp_scen$Density[i]
-    
-    if (density == "Half") {
-      markerPosition_kb <- read_csv(paste0("Dataset/",dataset,"/50_markerPositionsRep.res.bz2"),show_col_types = FALSE) %>%
-        mutate(position_kb = BPP/1000)
-      geno_length <-  markerPosition_kb$position_kb[nrow(markerPosition_kb)] - markerPosition_kb$position_kb[1]
-      N_SNPs <- nrow(markerPosition_kb)
-    }else{
-      markerPosition_kb <- read_csv(paste0("Dataset/",dataset,"/R_output/markerPosition_kb.txt"),show_col_types = FALSE)%>%
-        mutate(position_kb = position_bpp/1000)
-      
-      geno_length <-  markerPosition_kb$position_kb[nrow(markerPosition_kb)] - markerPosition_kb$position_kb[1]
-      N_SNPs <- nrow(markerPosition_kb)
-    }
-    
-    
-    ROH_edit <- read_csv(paste0("Dataset/",dataset,"/R_output/",density,"_Dens/",setting,"/ROH_edit.txt"),show_col_types = FALSE)
-    
-    # Make the BINS
-    
-    bin_vec <- vector()
-    
-    # Define breakpoints and labels
-    breakpoints <- c(0, 1000, 2000, 3000, 4000, 5000, 9000, Inf)
-    labels <- c("0_1MB", "1_2MB", "2_3MB", "3_4MB", "4_5MB", "5_9MB", "9MB+")
-    
-    # Use cut to create the bin_vec
-    bin_vec <- cut(ROH_edit$KB, breaks = breakpoints, labels = labels, right = FALSE)
-    
-    bin_vec_fac_2 <- factor(bin_vec, levels = c("0_1MB", "1_2MB" , "2_3MB" , "3_4MB" , "4_5MB","5_9MB","9MB+" ))
-    
-    ROH_edit$BIN <- bin_vec_fac_2
-    
-    
-    # summarise
-    ROH_ind <- ROH_edit %>% 
-      group_by(ID, BIN) %>% 
-      summarise(ROH_SNPs = sum(NSNP),
-                sum_kb = sum(KB),
-                F_ROH_snp = ROH_SNPs/N_SNPs,
-                F_ROH_kb = sum_kb/geno_length) %>% 
-      mutate(dataset = dataset, 
-             density = density,
-             setting = setting)
-    
-    ROH_ind_all <- rbind(ROH_ind_all, ROH_ind)
-    
-  } #set_dens
-  cat(dataset, "\n")
-}
+  ROH_ind <- rbind(ROH_ind,F_tot)
+  return(ROH_ind) 
+} #set_dens
+
+ROH_ind_all <- do.call(rbind, ROH_ind_all_list)
 
 
 # Here is the final funny little part
@@ -229,16 +174,19 @@ for (dataset in unique(scenario_tbl$Dataset)  ) {
 # parametersettings, density, BIN etc
 # So here I use the Complete() to make all the missing combinations 
 
+
+
+
 tester <- ROH_ind_all %>% 
-  group_by(dataset,density,setting ) %>% 
+  group_by(NE,Rep,Dens,Method) %>% 
   complete(ID, BIN ) %>% 
-  mutate_at(c(1:9), ~replace_na(.,0))
+  mutate_at(c(1:9), ~ replace_na(.,0))
 
 #then join it all together 
-corr_basis_BIN  <- left_join( tester,IBD_ind_all, by = c("ID", "BIN","dataset")) %>% 
+corr_basis_BIN  <- left_join( tester,IBD_ind_all, by = c("ID", "BIN","NE","Rep")) %>% 
   replace(is.na(.), 0)
 
-write.table(x = corr_basis_BIN ,file= paste("./Dataset/Correlation/F_ROH_IBD_BIN.txt", sep = ""),row.names=FALSE,quote=FALSE)
+write.table(x = corr_basis_BIN ,file= file.path(proj.dir,"Results/F_ROH_IBD_BIN.txt"),row.names=FALSE,quote=FALSE)
 
 
 
--- ROH_vs_IBD.R
+++ ROH_vs_IBD.R
@@ -1,341 +1,97 @@
 
 #libraries
-
+.libPaths("/mnt/users/odwa/R/myLib" )
 library(stringi)
 library(tidyverse)
 library(data.table)
-
-# In the slurm I'm feeding giving it a three column text file 
-# arg1 = dataset, arg2 = density, "arg 3 = parameters. 
-
-args <- commandArgs(trailingOnly = TRUE)
-
-
-dataset  <- args[1]
-density <- args[2]
-parameters <- args[3]
-
-
-# dataset  <- "NE200_100cm_rep0"
-# density <- "Full"
-# parameters <- "Meyerman"
-
-
+library(foreach)
+#library(snow)
+library(doSNOW)
+library(itertools)
+library(doParallel)
+library(parallel)
 # =============
 #   Directories
 # =============
 
-roh.dir = paste0("Dataset/",dataset,"/PLINK_out/",density,"_Dens/",parameters)
-ibd.dir = paste0("Dataset/",dataset,"/IBD_segments")
-R.dir = paste0("Dataset/",dataset,"/R_output/",density,"_Dens/",parameters)
-
-
-dir.create(paste0("Dataset/",dataset,"/R_output/",density,"_Dens/") )
-
-dir.create(R.dir)
-
-
+data.dir <- paste0("/mnt/project/SimData/Paper_1")
 
 # =============
-#   loading basic files.  
+#  Combos 
 # =============
-marker_pos <- read_csv(paste0("Dataset/",dataset, "/R_output/markerPosition_kb.txt"),show_col_types = FALSE )
-obs_QTL  <- read_csv(paste0("Dataset/",dataset, "/R_output/All_ind_obs_QTL.res.bz2"), 
-                     col_types = cols(geno = col_character() ), show_col_types = FALSE) 
 
-true_IBD <- read_csv(paste0("Dataset/",dataset, "/R_output/All_Ind_true_IBD.res.bz2"), show_col_types = FALSE )
-IBD_pos <- read.csv(paste0("Dataset/",dataset, "/R_output/true_IBD_position.txt") )
+NE = c("NE100","NE200")
+Rep = c(1:10)
+Dens = c("Full_Dens","Half_Dens")
+Method = c("Meyermans","Default","Norm", "Norm_small")
 
-ROH_raw <- read_table(paste0(roh.dir, "/ROH_analyse.hom") )
+combinations <- expand.grid(NE = NE , Rep = Rep, Dens = Dens ,Method = Method)
 
-ped <- read_table(paste0("Dataset/",dataset, "/pedigree_rel.generations.txt"))
-
-# ==========
-#   Editing the ROH file
-# ========
-
-ROH_inf <- ROH_raw %>% 
-  select(c(IID, SNP1,SNP2, POS1, POS2,KB, NSNP)) %>% 
-  dplyr::rename(ID = IID) %>% 
-  group_by(ID) %>% 
-  mutate(run = dplyr::row_number())
-
-rm("ROH_raw")
-write.table(ROH_inf, file = paste0(R.dir ,"/ROH_edit.txt"), row.names = FALSE, sep = ",", quote = FALSE )
-
-
-
-# ========
-#   Write a binary genome with ROH state 
-#   1 marker in ROH, 0 as non-ROH 
-# =======
-
-ROH_inf <- ROH_inf %>% 
-  filter(ID %in% ped$id) # select out only the analysis individuals 
-
-ID_vec <- unique(ROH_inf$ID)
-
-
-ROH_pos <- data.frame()
-
-for (ID in ID_vec) {
-  genome <- as.character( array(data = 0, dim = nrow(marker_pos)))  
-  
-  ROH_inf_ind <- ROH_inf[ROH_inf$ID %in% ID, ] # select out all ROH for one ID
-  ind_run_vec <- c(1:nrow(ROH_inf_ind))
-  NSNP <- 0
-  # Then for all markers that are within a run put a 1 on the genome 
-  for (run in ind_run_vec) {
-    start <- ROH_inf_ind$POS1[run]
-    stop <- ROH_inf_ind$POS2[run]
-    #find markers in ROH and write 1s
-    rel_marker <- marker_pos$tmp_marker[marker_pos$position_bpp >= start & marker_pos$position_bpp <= stop]
-    genome[rel_marker] <- paste(1)
-
-    NSNP <- NSNP+ length(rel_marker)
-    
-  }# RUN
-  NNA <- sum(is.na(genome))
-  genome_str <-  paste0(genome, collapse = " ")   
-  
-  genome_id <- data.table(
-    ID = ID,
-    genome = genome_str,
-    NSNP = NSNP,
-    NNA = NNA
-  )
-  ROH_pos <- rbind(ROH_pos, genome_id)
-  #cat("we're on ind", ID,"\n")
-} # ID 
-
-cat("ROH bin found \n")
-
-PLINK_ROH_bin <- ROH_pos
-
-
-#write_csv(ROH_pos, paste0( R.dir, "/PLINK_ROH_bin_ROH_ind.res.bz2"), quote = c("none"))   
-
-
-rm(list = setdiff(ls(envir = globalenv()), c("ped","dataset","density","parameters" ,"file.dir",
-                                             "ibd.dir","res.dir","R.dir","roh.dir","ID_vec","PLINK_ROH_bin","marker_pos",
-                                             "ROH_inf")), envir = globalenv())
-
-
-
-# ===================
-# =============
-#  ROH|IBD - Power
-# =============
+# ==================
+#
+#   Benchmarking 
+#
 # ===================
 
+# Number of cores to use
+num_cores <- as.integer(Sys.getenv("SLURM_CPUS_PER_TASK", unset = 1))
+print(paste0("CPUs available: ", num_cores))
+# Use one less than available cores
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+clusterEvalQ(cl, {
+  .libPaths("/mnt/users/odwa/R/myLib" )
+  library(stringi)
+  library(tidyverse)
+  library(data.table)
+  library(foreach)
+  #library(snow)
+  library(doSNOW)
+  library(doParallel)
+  library(parallel)
+})
 
-# ===========
-#   datasets 
-# ===========
+source("/mnt/users/odwa/paper-1/ROH-vs-IBD/IBD_vs_ROH_functions.R")
 
-info_ADAM_ROH_bin <- read.csv(paste0(res.dir, "/info_ADAM_roh_bin.txt") )
-info_ADAM_ROH_bin <- info_ADAM_ROH_bin %>% 
-  filter(ID %in% ped$id) %>% 
-  mutate(start_kb =start_bpp/1000,
-         stop_kb = stop_bpp/1000)
-
-
-ID_vec <- unique(info_ADAM_ROH_bin$ID)
-ID_plink <- unique(PLINK_ROH_bin$ID)
-
-# In the first generations not all individuals have IBD segments 
-# So here I create "empty" genomes for these IDs to make sure all runs normally 
-leftovers <- ID_vec[!ID_vec %in% ID_plink]
-gen <- (paste0(as.character( array(data = 0, dim = nrow(marker_pos))), collapse = " "))
-len <- length(leftovers)
-
-ext <- data.frame(ID = leftovers, 
-                  genome = rep(gen,len),
-                  NSNP = rep(0,len), 
-                  NNA = rep(0,len))
-
-PLINK_ROH_bin <- rbind(PLINK_ROH_bin, ext)
-
-rm("leftovers","gen","len","ext")
-
-PLINK_ROH_bin <- PLINK_ROH_bin %>% 
-  distinct(ID, .keep_all = TRUE)
-
-# ============
-# ROH|IBD  - loop
-# =============
-
-final <- data.frame( ID= character(0), run = character(0), result = character(0)) 
-
-
-for (i in ID_vec){
+# Parallel loop
+foreach(i = 1:nrow(combinations), .packages = c('data.table', 'dplyr', 'readr',"tidyr"),
+        .export = c("RohBinGeno","RohPower","RohTruePos")) %dopar% {
   
-  IBD_ind  <- PLINK_ROH_bin %>% 
-    filter(ID == i) %>% 
-    pull(genome)
+  # Get the current combination
+  NE <- combinations$NE[i]
+  Rep <- combinations$Rep[i]
+  Dens <- combinations$Dens[i]
+  Method <- combinations$Method[i]
+
   
-  info_ind <- info_ADAM_ROH_bin %>% 
-    filter(ID == i)
+  data.input.dir <- paste0(data.dir, "/",NE, "/Rep",Rep)
+  roh.dir <- paste0(data.input.dir,"/",Dens,"/",Method)
   
-  ind_run_vec <- c(1:nrow(info_ind))
-  ind_result <- data.frame(matrix(NA, nrow = nrow(info_ind), ncol = 3))
-  colnames(ind_result) <- c("ID","ROH","result")
-  ind_result$ID <- info_ind$ID
+  marker_pos <- read_csv(paste0(data.input.dir, "/markerPosition_kb.txt"), show_col_types = FALSE)
+  true_IBD <- read_csv(paste0(data.input.dir, "/All_Ind_true_IBD.res.bz2"), show_col_types = FALSE)
+  IBD_pos <- read.csv(paste0(data.input.dir, "/true_IBD_position.txt"))
+  ped <- read_table(paste0(data.input.dir, "/pedigree_rel_generations.txt"))
+  obs_IBD <- read_csv(paste(data.input.dir, "/extend_bin_ADAM_ROH.res.bz2", sep = ""))
+  obs_homo <- read_csv(paste0(data.input.dir, "/all_ind_observed_homozyg_QTL.res.bz2"))
   
-  segment <- strsplit(IBD_ind, " ")[[1]] # split genome string into a vector 
-  segment <- segment[!grepl("^\\s*$", segment)]  #remove all the empty chunks 
-  for(run in ind_run_vec) {
-    ind_result$ROH[run] <- run # Store what per ID loop it is 
-    
-    start <- info_ind$start_bpp[run] 
-    stop <- info_ind$stop_bpp[run]
-    
-    # Since the IBD segments are found with a text string function, 
-    # Some of the registered segements have only 1 marker. So first i exclude them
-    if( start == stop){cat("no run", i, run ,"\n")
-    }else{
-      rel_marker <- marker_pos$tmp_marker[marker_pos$position_bpp >= start & marker_pos$position_bpp <= stop]
-      
-      # The next issue is that for very short runs sometimes we wont have a 
-      # SNP marker between start and stop. these areas are then removed 
-      if(length(rel_marker) == 0){cat("no markers", i, run ,"\n")
-      }else{
-        # To be able to compare the results to the raw data. I still write the results 
-        # Into a genome string format. While ineficient, it  10/10 for troubleshooting 
-        run_res <- vector()
-        for (marker in rel_marker) {
-          
-          if (segment[marker] == 1){
-            run_res_t<- paste(1)  #is correct 
-          } else {
-            run_res_t <- paste(0)
-          }# else 
-          run_res <- c(run_res, run_res_t)
-        } #pos  
-        ind_result$result[run] <- paste0(run_res, collapse = " ")
-      } # else rel_mark
-    } # else start stop
-  } # run 
-  final <- rbind(final, ind_result)
-  cat(i,"\n")
-} # IDs
-
-# then I examine all the genome strings and cook it down to per. segment numbers
-final$Length_SNP <- (stringi::stri_length(final$result) - stringi::stri_count_fixed(final$result, " "))
-final$sum <- sapply(strsplit(final$result," "), function(x) sum(as.numeric(x)))
-final$pros_right <- final$sum/final$Length_SNP*100
-
-write_csv(final, paste0(R.dir, "/TRUE_ADAM_by_PLINK.txt"), quote = "none")    
-
-
-
-
-
-
-# ===========
-# IBD|ROH - True positive
-# ==========
-
-obs_IBD <- read_csv(paste(ibd.dir, "/extend_bin_ADAM_ROH.res.bz2", sep = "") )
-
-obs_homo <- read_csv(paste0("Dataset/",dataset, "/R_output/all_ind_observed_homozyg_QTL.res.bz2", sep = "") )
-
-
-# There is not a 100% match between what IDs are seen in the two datasets 
-# So here I'm just finding out who's missing and giving them a completely 
-# non-IBD 00000... genome 
-
-leftovers <- obs_homo$ID[!obs_homo$ID %in% obs_IBD$ID]
-gen <- (paste0(as.character( array(data = 0, dim = nrow(marker_pos))), collapse = " "))
-len <- length(leftovers)
-
-ext <- data.frame(ID = leftovers, 
-                  genome = rep(gen,len))
-obs_IBD <- rbind(obs_IBD, ext)
-obs_IBD <- obs_IBD %>% 
-  distinct(ID, .keep_all = TRUE)
-
-
-run_vec <- 1:nrow(ROH_inf)
-all_run_vec <- nrow(ROH_inf)
-all_runs <- data.frame()
-
-
- # looping it over ROHs
-for (run in run_vec) {
-  ID <- ROH_inf$ID[run]
-  run_id <- ROH_inf$run[run] # ROH info
-  homo_gen <- obs_homo$genome[obs_homo$ID %in% ID] # indi. homozyg. state
-  homo_geno <- strsplit(homo_gen, " ")[[1]] 
+  info_ADAM_ROH_bin <- read.csv(paste0(data.input.dir, "/info_ADAM_roh_bin.txt")) %>%
+    filter(ID %in% ped$id) %>%
+    mutate(start_kb = start_bpp / 1000,
+           stop_kb = stop_bpp / 1000)
   
-  IBD_gen <- obs_IBD$genome[obs_IBD$ID %in% ID] # Indi. IBD state
-  IBD_geno <- strsplit(IBD_gen, " ")[[1]] 
-  
-  run_inf <- ROH_inf[run, ]
-  
-  start <- run_inf$SNP1
-  stop <- run_inf$SNP2
-  
-  rel_markers <-  marker_pos$tmp_marker [marker_pos$locus >= start & marker_pos$locus <= stop]
-  
-  run_markers <- vector()
-  for (marker in rel_markers) {
-    # number code:
-    #  0 = HET
-    #  1 = Non HOMO IBD -> can be the masked markers
-    #  2 = IBS / AIS 
-    #  3 = IBD 
-    #  4 = something's wrong 
-    if (homo_geno[marker] == "1" && IBD_geno[marker] == "1"){
-      run_mark <- paste0("3")  # it is IBD 
-    }else if (homo_geno[marker] == "1" && IBD_geno[marker] == "0"){
-      run_mark <- paste0("2")  # it is IBS 
-    }else  if (homo_geno[marker] == "0" && IBD_geno[marker] == "1"){
-      run_mark <- paste0("1")  # it is Non HOM IBD  
-    }else if (homo_geno[marker] == "0" && IBD_geno[marker] == "0"){ 
-      run_mark<- paste0("0")  # it is hetero 
-    }else{ 
-      run_mark<- paste0("4")  # something's WRONG 
-    } # else 
-    run_markers <- c(run_markers, run_mark)
-  }# marker 
-  run_results <- data.frame(ID = ID, 
-                            ROH = run_id, 
-                            results = paste0(run_markers, collapse = " "), 
-                            N_SNPs = length(rel_markers))
-  all_runs <- rbind(all_runs, run_results)
-  cat(round(run/all_run_vec*100, digits = 2), "% \n")
-} # run 
 
+  ROH_inf <- read_table(paste0(roh.dir, "/ROH_analyse.hom")) %>%
+      select(c(IID, SNP1, SNP2, POS1, POS2, KB, NSNP)) %>%
+      dplyr::rename(ID = IID) %>%
+      filter(ID %in% ped$id) %>%
+      group_by(ID) %>%
+      mutate(run = row_number())
 
-
-# Function to count unique characters in a string
-count_unique <- function(string) {
-  counts <- table(strsplit(gsub(" ", "", string), ""))
-  counts_df <- as.data.frame(counts)
-  colnames(counts_df) <- c("Number", "Count")
+  PLINK_ROH_bin <- RohBinGeno(ROH_inf, marker_pos)
+  RohPower(info_ADAM_ROH_bin, PLINK_ROH_bin, marker_pos, tmp.dir = roh.dir)
+  RohTruePos(ROH_inf, obs_IBD, obs_homo, marker_pos, tmp.dir = roh.dir)
   
-  # Set all numbers from 0 to 4 with count 0 if not present
-  all_numbers <- as.character(0:4)
-  counts_df <- counts_df %>%
-    complete(Number = all_numbers, fill = list(Count = 0))
-  
-  return(counts_df)
-}
+} # End of foreach loop
 
-# Apply the count_unique function to all rows and combine results into a data frame
-combined_result <- all_runs %>%
-  group_by(ID, ROH) %>%
-  summarise(result = list(count_unique(results))) %>%
-  unnest(result) %>%
-  pivot_wider(names_from = Number, values_from = Count, values_fill = 0)
-
-# Print the combined result
-# and rename the columns
-all_results <- left_join(all_runs, combined_result, by = c('ID', 'ROH') ) %>% 
-  rename(Hetero = "0",
-         N_HOM_IBD = "1", 
-         IBS = "2", 
-         IBD = "3")
-
-write_csv(all_results,paste(R.dir, "/PLINK_ROH_IBD_vs_IBS.res.bz2", sep=""), quote = c("none"))
+# Stop the cluster after processing
+stopCluster(cl)
